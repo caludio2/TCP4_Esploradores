6276bfac835906eef9a24df1b908a087e21650cc
claudioCruz <claudiocruz109399@gmail.com>
2025-09-16 10:10:12 -0300

    firt try

--- TCP4/Assets/CameraMoviment.cs
+++ TCP4/Assets/CameraMoviment.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class CameraMoviment : MonoBehaviour
 {
+    public float maxVariation = 0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Input.location.Start();
+        Input.gyro.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(Input.gyro.rotationRate);
+        Vector3 rotationRate = Input.gyro.rotationRate;
+        transform.Rotate(0, -rotationRate.y * Mathf.Rad2Deg / 15.25f, 0);
+        if(Input.acceleration.x > maxVariation)
+        {
+            transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+        }
     }
 }
