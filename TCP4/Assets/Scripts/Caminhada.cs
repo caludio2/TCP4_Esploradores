a715f29c7322e089b893b16cc81537caf620baac
claudioCruz <claudiocruz109399@gmail.com>
2025-09-18 15:15:15 -0300

    comit camera moviment caminhada

--- TCP4/Assets/Scripts/Caminhada.cs
+++ TCP4/Assets/Scripts/Caminhada.cs
@@ -4,10 +4,8 @@ using TMPro;
 
 public class Caminhada : MonoBehaviour
 {
-    public Slider slider;
     public TMP_Text text;
-    public float maxVariation = 0.5f; //<== VARIACAO MINIMA QUE SERA DETECTADA PELO SENSOR DO CELULAR
-    public float sensibilidade = 0.5f;
+    private bool calibrado;
     void Start()
     {
         Input.gyro.enabled = true; //<== APRONTANDO O SENSOR PARA SER USADO
@@ -15,19 +13,9 @@ public class Caminhada : MonoBehaviour
 
     void Update()
     {
-        sensibilidade = slider.value;
-        text.text = "Sensibilidade " + sensibilidade;
-        Vector3 rotationRate = Input.gyro.rotationRate; //<== PASSANDO PARA UMA VARIAVEL QUE PODEREMOS ALTERAR COM MAIS FACILIDADE
-        transform.Rotate(0, (-rotationRate.y * sensibilidade) * Mathf.Rad2Deg , 0); //<== APLICANDO A ROTACAO NO OBJETO
-
-        transform.position = new Vector3(transform.position.x , Input.acceleration.x , transform.position.z); //<== MOVENDO A CAMERA PARA CIMA E PARA BAIXO BASEADO NO MOVIMENTO DO PLAYER
-        if(Input.acceleration.x > maxVariation) //<== CHECANDO SE A VARIACAO FOI SUFICIENTE PARA SER DETECTADA
-        {
-            transform.Translate(Vector3.forward * 5 * Time.deltaTime); //<== ACAO DE MOVIMENTAR O PLAYER PARA FRENTE A CADA VARIACAO DO SENSOR 
-
-            //PROBLEMATICA A SER RESOLVIDA TALVEZ:
-            //O PLAYER PODE DAR PASSOS MUITO LONGOS CASO ELE CONTINUE MOVENDO O CELULAR PARA CIMA ISSO NAO E UM PASSO
-            //TALVEZ UMA SOLUCAO PODERIA SER BOTAR UM NUMERO DE VARIACAO MAXIMA
-        }
+        
+            text.text = "Calibrado";
+            transform.rotation = Quaternion.Euler(-Input.gyro.attitude.x * 100,-Input.gyro.attitude.y * 100,Input.gyro.attitude.z * 100);
+            print(new Vector3(Input.gyro.attitude.x,Input.gyro.attitude.y,Input.gyro.attitude.z));
     }
 }
