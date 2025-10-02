05fb78b8a455c28d80ab12926091f06ebbc25234
claudioCruz <claudiocruz109399@gmail.com>
2025-10-02 08:30:50 -0300

    trilha

--- TCP4/Assets/Scripts/SplineScript.cs
+++ TCP4/Assets/Scripts/SplineScript.cs
@@ -6,41 +6,41 @@ public class SplineScript : MonoBehaviour
 {
     public SplineAnimate spline; //Componente responsavel por mover o personagme
 
-    float forçaMaxima = 2;//Quantidade que sera alterada pela dificuldade do terreno
-    float forçaMinima = 0;//Quantidade que sera alterada pela dificuldade do terreno
+    float forcaMaxima = 2;//Quantidade que sera alterada pela dificuldade do terreno
+    float forcaMinima = 0;//Quantidade que sera alterada pela dificuldade do terreno
 
-    bool tropeço = true;//Checagem de se o player tropeçou / stun
+    bool tropeco = true;//Checagem de se o player tropeï¿½ou / stun
 
     [Range(-100f, 100f)]
-    public float multiplicadorDetempo = 1;//Tempo que o player tera que esperar até levantar novamente
+    public float multiplicadorDetempo = 1;//Tempo que o player tera que esperar atï¿½ levantar novamente
 
     void Update()
     {
-        if (tropeço)//checa se o jogador nao esta caido
+        if (tropeco)//checa se o jogador nao esta caido
         {
-            if (Input.acceleration.y > forçaMinima && Input.acceleration.y < forçaMaxima)//checa se o jogador fez o movimento certo
+            if (Input.acceleration.y > forcaMinima && Input.acceleration.y < forcaMaxima)//checa se o jogador fez o movimento certo
             {
-                spline.Play();//Da play na animaçao da spline
+                spline.Play();//Da play na animaï¿½ao da spline
             }
-            else if(Input.acceleration.y > forçaMaxima)//Stuna o player caso ele nao seja cuidadoso
+            else if(Input.acceleration.y > forcaMaxima)//Stuna o player caso ele nao seja cuidadoso
             {
-                spline.Pause();//Pausa a animaçao da spline
+                spline.Pause();//Pausa a animaï¿½ao da spline
                 StartCoroutine(EsperarEExecutar());//Contador de tempo para o stun
             }
 
             else //Caso ele nao caia nos ifs  anteriores ele deve ficar parado
             {
-                spline.Pause();//Pausa a animaçao da spline
+                spline.Pause();//Pausa a animaï¿½ao da spline
             }
         }
     }
     
     IEnumerator EsperarEExecutar() //Conta o tempo e retorna o feedback de tempo usado
     {
-        tropeço = false;
-        print("Você caiu por : " + 1 * multiplicadorDetempo + "segundos");
+        tropeco = false;
+        print("Vocï¿½ caiu por : " + 1 * multiplicadorDetempo + "segundos");
         yield return new WaitForSeconds(1 * multiplicadorDetempo);
         print("voce levantou");
-        tropeço = true;
+        tropeco = true;
     }
 }
